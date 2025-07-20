067ad5520363b30bf3c1805348dc365787453a9c
ruribou <ruribou@hotmail.com>
2025-07-20 20:23:34 +0900

    test: テストケースの記述

---
+++ internal/log/store_test.go
@@ -0,0 +1,31 @@
+package log
+
+import (
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	write = []byte("hello world")
+	width = uint64(len(write)) + lenWidth
+)
+
+func TestStoreAppendRead(t *testing.T) {
+	f, err := os.CreateTemp("", "store_append_read_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+
+	s, err := newStore(f)
+	require.NoError(t, err)
+
+	testAppend(t, s)
+	testRead(t, s)
+	testReadAt(t, s)
+
+	s, err = newStore(f)
+	require.NoError(t, err)
+	testRead(t, s)
+}
